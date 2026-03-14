c84139f6c92a061c78989fc3f0f7990863c01077
numenavi <numenavi@outlook.com>
2026-03-14 14:57:42 +0100

    Fixed some bugss

--- lib/admin.ts
+++ lib/admin.ts
@@ -3,6 +3,7 @@ import { auth } from "@clerk/nextjs/server"
 const allowedIds = [
     "user_394uMn4YBAGbSp3w9AFhQxUdxeF",
     "user_39A0SteccTzKrcFD2xK0zTPL4yf",
+    "user_3AuEBemG6a4OLnaTfqYE8doeYyp",
 ];
 
 export const isAdmin = async () => {
